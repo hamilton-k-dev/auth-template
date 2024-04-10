47365cfd0f4b83a3cd22321dc8c2c470e414f9e0
hamilton-k-dev <hamiltonkenfack@gmail.com>
2024-04-10 13:38:31 +0100

    translation added on email

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,9 +1,9 @@
+import { getTranslations } from "next-intl/server";
 import nodemailer from "nodemailer";
-import  handlebars from "handlebars";
-import { testTemplate } from "@/template/test";
 const { SMTP_PASSWORD, SMTP_EMAIL } = process.env;
 
-export const sendMail = async ({ from, to, name, subject, body }:any) => {
+export const sendMail = async ({ from, to, name, subject, body }: any) => {
+  const t = await getTranslations("email")
   try {
     // Create a transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -18,60 +18,55 @@ export const sendMail = async ({ from, to, name, subject, body }:any) => {
 
     // Send mail with defined transport object
     let info = await transporter.sendMail({
-      from: `"Your Name" ${SMTP_EMAIL}`, // sender address
+      from: `"Next.js Template" ${SMTP_EMAIL}`, // sender address
       to, // list of receivers
       subject, // Subject line
       html: body, // HTML body content
     });
 
     console.log("Message sent: %s", info.messageId);
-    return { success: "mail send" };
+    return { success: t("emailS") };
   } catch (error) {
     console.error("Error sending email", error);
-    return { error: "erro trying to send mail" };
+    return { error: t("sendingME") };
   }
 };
-export function compileTestTemplate(name:string) {
-  const template = handlebars.compile(testTemplate);
-  const htmlBody = template({
-    name: name,
-  });
-  return htmlBody;
-}
 
-export const sendPasswordResetEmail = async (email:string, token:string) => {
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const t = await getTranslations("email")
   try {
     await sendMail({
       to: email,
       from: SMTP_EMAIL,
-      subject: "Reinitialisez votre mot de passe",
-      body: `<p>your email reset token is ${token}</p>`,
+      subject: t("resetP"),
+      body: `<p>${t("resetT")} ${token}</p>`,
     });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const sendVerificationEmail = async (email:string, token:string) => {
-  const confirmLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification?token=${token}`;
+export const sendVerificationEmail = async (email: string, token: string) => {
+  const t = await getTranslations("email")
   try {
     await sendMail({
       from: SMTP_EMAIL,
       to: email,
-      subject: "Confirmez votre address mail",
-      body: `<p>This is the token to verified your email <strong>${token}</strong></p>`,
+      subject: t("confirmE"),
+      body: `<p>${t("emailVT")} <strong>${token}</strong></p>`,
     });
   } catch (error) {
     console.log(error);
   }
 };
-export const sendTwoFactorTokenEmail = async (email:string, token:string) => {
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  const t = await getTranslations("email")
   try {
     await sendMail({
       from: SMTP_EMAIL,
       to: email,
-      subject: "2FA code",
-      body: `<p>votre code de 2fa est : ${token}</p>`,
+      subject: t("2fa"),
+      body: `<p>${t("2faCode")} : ${token}</p>`,
     });
   } catch (error) {
     console.log(error);
